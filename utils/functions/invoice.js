87a8b28e743e2f734042a86ed4ea53d6d5b16dba
akanz <samuelakande2014@yahoo.com>
2023-08-27 19:02:57 +0100

    invoice model, set receiverEmail to not unique

--- utils/functions/invoice.js
+++ utils/functions/invoice.js
@@ -34,7 +34,9 @@ function generateInvoicePDF (sender, receiverEmail, items, description, schedule
     (async () => {
       try {
         // Launch Puppeteer
-        const browser = await puppeteer.launch()
+        const browser = await puppeteer.launch({
+          headless: false
+        })
         const page = await browser.newPage()
 
         // Generate PDF from the HTML content
